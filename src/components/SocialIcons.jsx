cb758d4c33b70396d61904c035766b8d8bf8d119
rvp15 <vedhagithub@gmail.com>
2023-01-12 11:43:04 -0500

    add social icons

--- src/components/SocialIcons.jsx
+++ src/components/SocialIcons.jsx
@@ -1,15 +1,72 @@
-import React from 'react'
-import{BsGithub,BsLinkedin,BsPersonLinesFill} from 'react-icons/bs'
-import {HiOutlineMail} from 'react-icons/hi'
+import React from "react";
+import {
+  BsFillPersonLinesFill,
+  BsGithub,
+  BsLinkedin,
+  BsPersonLinesFill,
+} from "react-icons/bs";
+import { HiOutlineMail } from "react-icons/hi";
 function SocialIcons() {
+  const links = [
+    {
+      id: 1,
+      link: (
+        <>
+          LinkedIn <BsLinkedin size={20} />
+        </>
+      ),
+      href: "https://www.linkedin.com/in/vedhapriya-rajagopal-82a702232/",
+      style: "rounded-tr-md",
+    },
+    {
+      id: 2,
+      link: (
+        <>
+          GitHub <BsGithub size={20} />
+        </>
+      ),
+      href: "https://github.com/rvp15",
+    },
+    {
+      id: 3,
+      link: (
+        <>
+          Mail <HiOutlineMail size={20} />
+        </>
+      ),
+      href: "mailto:foo@gmail.com",
+    },
+    {
+      id: 4,
+      link: (
+        <>
+          Resume <BsFillPersonLinesFill size={20} />
+        </>
+      ),
+      href: "/resume.pdf",
+      style: "rounded-br-md",
+      download: true,
+    },
+  ];
+  // flex justify-between items-center w-40 h-14 px-4 bg-gray-500 ml-[-100px] hover:ml-[-10px] hover:rounded-md duration-300
+//lg:flex is media Q for 1024px
   return (
-    <div className='flex-col top-[35%] left-0 fixed'>
-        <ul>
-            <li className='flex justify-between items-center w-40 h-14 px-4 bg-gray-500 ml-[-100px] hover:ml-[-10px] hover:rounded-md duration-300'><a className='flex justify-between items-center w-full text-white' href=""><>
-           LinkedIn <BsLinkedin size={30}/></></a></li>
-        </ul>
+    <div className=" hidden lg:flex top-[35%] fixed left-0 flex-col">
+      <ul>
+        {links.map(({id,link,href,style,download}) => (
+          <li key={id} className={"w-40 h-14 px-4 py-3 bg-sky-500/50 ml-[-100px] hover:ml-[-10px] hover:rounded-md duration-300 hover:bg-cyan-600" + '' + style}>
+            <a
+              className="flex justify-between items-center w-full text-white"
+              href={href}
+              download={download}
+              target='_blank'  
+              rel="noreferrer" 
+            >{link}</a>
+          </li>
+        ))}
+      </ul>
     </div>
-  )
+  );
 }
 
-export default SocialIcons
+export default SocialIcons;
